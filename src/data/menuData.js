a60c82351d797453611d511b33db93b76188e15e
davinson garcia <davinson_09@hotmail.com>
2023-01-27 13:56:54 -0400

    Delete src directory

--- src/data/menuData.js
+++
@@ -1,7 +0,0 @@
-export const menuData=[
-    {title: 'Shop', link : '/Shop'},
-    {title: 'More', link : '/More'},
-    {title: 'Labs' ,link : '/Labs'},
-    {title: 'Whohesale', link : '/Whohesale'},
-    {title: 'Account', link : '/Account'},
-];
